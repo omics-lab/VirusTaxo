bbc25885f5616e40e27c534bd2a153878793946d
Rajan Saha Raju <imrajon0@gmail.com>
2022-04-23 12:25:52 +0600

    metagenomics script updated

--- benchmark.py
+++ benchmark.py
@@ -28,74 +28,38 @@ if __name__ == '__main__':
         for dir in dirs:
 
             fasta_file = os.path.join(root, dir, dir + '.contigs.fa')
-            print(fasta_file)
+            # print(fasta_file)
             for record in SeqIO.parse(fasta_file, "fasta"):
+                def get_genus(seq):
+                    count = defaultdict(int)
+                    for idx in range(len(seq) - params['k'] + 1):
+                        kmer = seq[idx:idx + params['k']]
+                        if kmer in model:
+                            matched_genera = model[kmer]
+                            if params['discriminative'] and len(matched_genera) == 1:
+                                count[list(matched_genera)[0]] += 1
+                            else:
+                                for genus in matched_genera:
+                                    count[genus] += 1
+                    genus_score_tuple = list(count.items())
+                    if not genus_score_tuple:
+                        E = 1.0
+                        return -1, -1, E
+                    else:
+                        E = entropy(np.array([x for _, x in genus_score_tuple]))
+                        prediction = max(genus_score_tuple, key=lambda x: x[1])[0]
+                        cnt = max(genus_score_tuple, key=lambda x: x[1])[1]
+                        return prediction, cnt, E
+
                 seq = str(record.seq)
-                count = defaultdict(int)
-                for idx in range(len(seq) - params['k'] + 1):
-                    kmer = seq[idx:idx+params['k']]
-                    if kmer in model:
-                        matched_genera = model[kmer]
-                        if params['discriminative'] and len(matched_genera) == 1:
-                            count[list(matched_genera)[0]] += 1
-                        else:
-                            for genus in matched_genera:
-                                count[genus] += 1
-                genus_score_tuple = list(count.items())
-                if not genus_score_tuple:
-                    E = 1.0
+                rc = record.seq.reverse_complement()
+                # print(f'{record.id}')
+                prediction_1, cnt_1, E_1 = get_genus(seq)
+                prediction_2, cnt_2, E_2 = get_genus(rc)
+                if prediction_1 == -1 and prediction_2 == -1:
+                    print(f'{dir}\t{record.id}\t{len(seq)}\tUnclassified\t0\t1.0')
                 else:
-                    E = entropy(np.array([x for _, x in genus_score_tuple]))
-
-                if E > 0.5:
-                    print(f'{dir}\t{record.id}\t{len(seq)}\tUnclassified\t-\t-')
-                else:
-                    prediction = max(genus_score_tuple, key=lambda x: x[1])[0]
-                    cnt = max(genus_score_tuple, key=lambda x: x[1])[1]
-                    print(f'{dir}\t{record.id}\t{len(seq)}\t{prediction}\t{cnt}\t{E}')
-
-
-
-
-
-
-    # unclassified, classified = 0, 0
-    #
-    # for record in tqdm(SeqIO.parse(args.seq, format='fasta')):
-    #     if record.id not in accession_to_genus:
-    #         continue
-    #     # print(f'predicting for the id: {record.id}')
-    #     seq = str(record.seq)
-    #     count = defaultdict(int)
-    #
-    #     # genus = accession_to_genus[record.id]
-    #     for idx in range(len(seq) - params['k'] + 1):
-    #         kmer = seq[idx:idx+params['k']]
-    #         if kmer in model:
-    #             matched_genera = model[kmer]
-    #             if params['discriminative'] and len(matched_genera) == 1:
-    #                 count[list(matched_genera)[0]] += 1
-    #             else:
-    #                 for genus in matched_genera:
-    #                     count[genus] += 1
-    #     genus_score_tuple = list(count.items())
-    #
-    #     if not genus_score_tuple:
-    #         E = 1.0
-    #     else:
-    #         E = entropy(np.array([x for _, x in genus_score_tuple]))
-    #
-    #     if E > 0.5:
-    #         unclassified += 1
-    #     else:
-    #         prediction = max(genus_score_tuple, key=lambda x: x[1])[0]
-    #         if prediction == accession_to_genus[record.id]:
-    #             classified += 1
-    #
-    # total = len(accession_to_genus)
-    # # print(unclassified)
-    # # print(classified)
-    # # print("N\tUnclassified\tRemaining\tCorrectly_Classified\tAccuracy")
-    # n = total - unclassified
-    # print(f'{total}\t{unclassified}\t{n}\t{classified}\t{classified / n * 100}')
-
+                    if cnt_1 > cnt_2:
+                        print(f'{dir}\t{record.id}\t{len(seq)}\t{prediction_1}\t{cnt_1}\t{E_1}')
+                    else:
+                        print(f'{dir}\t{record.id}\t{len(seq)}\t{prediction_2}\t{cnt_2}\t{E_2}')
