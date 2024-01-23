30e93edcb07d99789b593860217df7258e1a1982
Rashedul <rashedul.gen@gmail.com>
2024-01-22 19:57:09 -0800

    data

--- temp.sh
+++ temp.sh
@@ -1,14 +1,4 @@
-# test
-
-python3 build.py \
-   --meta ./Dataset/RNA_meta.csv \
-   --seq ./Dataset/RNA_seq.fasta \
-   --k 17 \
-   --saving_path ./RNA.pkl
-
-python3 predict.py \
-   --model_path ./RNA.pkl \
-   --seq ./input.fasta
+#!/bin/bash
 
 # DNA
 python3 build.py \
@@ -19,14 +9,14 @@ python3 build.py \
 
 # RNA
 python3 build.py \
-   --meta ./Dataset/DNA_9384_meta.csv \
+   --meta ./Dataset/RNA_9067_meta.csv \
    --seq ./Dataset/sequences_20240122_3701960.fasta \
-   --k 21 \
-   --saving_path ./DNA_9384_k21.pkl 
+   --k 17 \
+   --saving_path ./RNA_9067_k17.pkl 
    
 # DNA_RNA
 python3 build.py \
    --meta ./Dataset/DNA_9384_meta.csv \
    --seq ./Dataset/sequences_20240122_3701960.fasta \
-   --k 21 \
-   --saving_path ./DNA_9384_k21.pkl 
+   --k 20 \
+   --saving_path ./DNA_RNA_18451_k20.pkl 
